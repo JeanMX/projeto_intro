13e323dce6d3c5bc3fe4765c5279b32d91f9d596
Jean Carlos <jean.xavier@thunderatz.org>
2019-11-27 17:13:42 -0300

    :bug: Fix bug

--- src/keypad.c
+++ src/keypad.c
@@ -59,56 +59,20 @@ bool keypad_read(char* c) {
         for (int j = 0; j < NUMBER_OF_LINES; j++) {
             HAL_GPIO_WritePin(pinos_linha[j].port, pinos_linha[j].pin, GPIO_PIN_RESET);
         }
-        // HAL_GPIO_WritePin(Line1_GPIO_Port, Line1_Pin, LOW);
-        // HAL_GPIO_WritePin(Line2_GPIO_Port, Line2_Pin, LOW);
-        // HAL_GPIO_WritePin(Line3_GPIO_Port, Line3_Pin, LOW);
-        // HAL_GPIO_WritePin(Line4_GPIO_Port, Line4_Pin, LOW);
 
         linha = i;
         HAL_GPIO_WritePin(pinos_linha[i].port, pinos_linha[i].pin, GPIO_PIN_SET);
 
-        // switch(ti){
-        //     case 3:
-        //         HAL_GPIO_WritePin(Line1_GPIO_Port, Line1_Pin, HIGH);
-        //         linha = 1;
-        //         break;
-        //     case 4:
-        //         HAL_GPIO_WritePin(Line2_GPIO_Port, Line2_Pin, HIGH);
-        //         linha = 2;
-        //         break;
-        //     case 5:
-        //         HAL_GPIO_WritePin(Line3_GPIO_Port, Line3_Pin, HIGH);
-        //         linha = 3;
-        //         break;
-        //     case 6:
-        //         HAL_GPIO_WritePin(Line4_GPIO_Port, Line4_Pin, HIGH);
-        //         linha = 4;
-        //         break;
-        // }
         for (int j = 0; j < NUMBER_OF_COLUMNS; j++) {
             if(HAL_GPIO_ReadPin(pinos_coluna[j].port, pinos_coluna[j].pin) == GPIO_PIN_SET){
                 coluna = j;
                 pressed = true;
+                while(HAL_GPIO_ReadPin(pinos_coluna[j].port, pinos_coluna[j].pin) == GPIO_PIN_SET){}
                 break;
             }
-            mcu_sleep(10);
+            mcu_sleep(1);
         }
 
-        // if(HAL_GPIO_ReadPin(Col1_GPIO_Port, Col1_Pin) == HIGH){
-        //     coluna = 1;
-        //     pressed = true;
-        // }
-        // if(HAL_GPIO_ReadPin(Col2_GPIO_Port, Col2_Pin) == HIGH){
-        //     coluna = 2;
-        //     pressed = true;
-        //     // while(HAL_GPIO_ReadPin(Col2_GPIO_Port, Col2_Pin) == HIGH){}
-        // }
-        // if(HAL_GPIO_ReadPin(Col3_GPIO_Port, Col3_Pin) == HIGH){
-        //     coluna = 3;
-        //     pressed = true;
-        //     // while(HAL_GPIO_ReadPin(Col3_GPIO_Port, Col3_Pin) == HIGH){}
-        // }
-        // mcu_sleep(10);
 
         if (pressed) break;
     }
